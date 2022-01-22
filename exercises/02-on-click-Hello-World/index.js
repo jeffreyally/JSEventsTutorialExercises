9f3ac9217eb3ac1eb5eb6b4db219d835f81965cf
jeffreyally <jeffreyally@gmail.com>
2022-01-22 21:50:05 +0000

    solution for 2 onclick Hello World

--- exercises/02-on-click-Hello-World/index.js
+++ exercises/02-on-click-Hello-World/index.js
@@ -1,1 +1,8 @@
-//Declare your function here
+function clickForHelloWorld() {
+    alert('Hello World!')
+  }
+
+  document.querySelector("#hello").onclick = clickForHelloWorld
+
+  //added the clickForHelloWorld function to the
+  //onclick property of the input/button element
