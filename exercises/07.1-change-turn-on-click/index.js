a6b6dd08657ead88a9e29b38e59ace75a56f6d1e
jeffreyally <jeffreyally@gmail.com>
2022-01-23 04:04:15 +0000

    solution for 7.1 chance turn on click

--- exercises/07.1-change-turn-on-click/index.js
+++ exercises/07.1-change-turn-on-click/index.js
@@ -4,12 +4,21 @@ window.onload = function loadfn() {
 	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
 }
 
-window.turnChanger = function turnChanger() {
+ function turnChanger() {
 	if (currentUser == "Mario") {
 		currentUser = "Juan";
-	} else {
+	} 
+	else if(currentUser == "Juan"){
+		currentUser = 'Josh'
+	}
+	else {
 		currentUser = "Mario";
 	}
 
 	document.getElementById("screen").innerHTML = "It's " + currentUser + "'s turn";
 }
+
+document.querySelector('button').onclick = turnChanger
+//removing windows.turnchanger and replacing it with the above also
+//worked. I think it is because instead of adding the function right away to the
+//windows object on load, it is added to the onclick property of the button
