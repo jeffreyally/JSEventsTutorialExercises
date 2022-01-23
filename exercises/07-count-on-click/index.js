13c97c34059696053b404a365dc0c2a6b5a27d70
jeffreyally <jeffreyally@gmail.com>
2022-01-23 03:50:11 +0000

    solution for 7 countonclick

--- exercises/07-count-on-click/index.js
+++ exercises/07-count-on-click/index.js
@@ -6,12 +6,16 @@ window.onload = function loadFunction()
 	//here I set the screen to the initial value when the website is fully loaded.
 	document.getElementById('screen').innerHTML = "The counter value is "+counter;
 }
-
-//called when the user clicks
-window.increaseCounter = function increaseCounter()
-{
-	//increase the global counter in one
-	counter++;
-	//update the screen with the new value
+function decreasing(){
+	counter--
 	document.getElementById('screen').innerHTML = "The counter value is "+counter;
 }
+
+document.querySelector('button').innerHTML = 'Decrease'
+//document.querySelector('button').onclick = decreasing
+//document.querySelector('button').addEvenlistener("click",decreasing)
+document.querySelector('button').addEventListener("click",()=>{
+	counter--
+	document.getElementById('screen').innerHTML = "The counter value is "+counter;
+})
+//commented lines work as well. Good practice to get familiar with syntax
