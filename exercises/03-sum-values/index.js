1a904ff708fd08450e4367238d7a5a357083fdc0
jeffreyally <jeffreyally@gmail.com>
2022-01-23 03:08:13 +0000

    solution for 3 sum values

--- exercises/03-sum-values/index.js
+++ exercises/03-sum-values/index.js
@@ -5,5 +5,11 @@ window.calculateSumListener = function() {
 	//Return the value of the input #secondNumber
 	var stringB = document.getElementById("secondNumber").value;
 
-	//your code goes here
+	document.querySelector('#resultNumber').value = parseInt(stringA)+parseInt(stringB)
 };
+
+console.log(document.querySelector('#resultNumber'))
+
+//I had thought of using innerHTML vs value but this link helped me
+//understand a bit better why that doesn't work 
+//https://stackoverflow.com/questions/31225901/difference-between-innerhtml-and-value-in-js
