a960549259f8ab844e766705f94139c96ab0c485
jeffreyally <jeffreyally@gmail.com>
2022-01-23 04:13:19 +0000

    solution for 8 event target

--- exercises/08-event-target/index.js
+++ exercises/08-event-target/index.js
@@ -1,6 +1,9 @@
 window.onload = function loadFn() {
 	var containerElm = document.getElementById("container");
-	containerElm.addEventListener("click", function(event) {
-		//you code here
+	containerElm.addEventListener("click", function(e) {
+		alert(''+e.target.id)
 	});
 };
+
+//Really cool alert. Above was a slightly lazy way to show the id of the target
+//but `${e.target.id}` works as well
